29a1b665223a2ff55a88f54d5dbc85733817aec4
Krishna Raman <kraman@gmail.com>
2015-06-08 17:16:04 -0700

    Wire up supervisor notifications

--- server/drivers/common/container.js
+++ server/drivers/common/container.js
@@ -3,13 +3,14 @@ var debug = require('debug')('strong-central:container');
 var lodash = require('lodash');
 var util = require('util');
 
-function Container(router, instanceId, env, token) {
+function Container(server, router, instanceId, env, token) {
   EventEmitter.call(this);
 
   this._id = instanceId;
   this._token = token;
   this._containerOptions = {};
   this._env = env || {};
+  this._server = server;
 
   this._channel = router.createChannel(
     this._onNotification.bind(this),
@@ -39,7 +40,6 @@ function getStartOptions() {
 }
 Container.prototype.getStartOptions = getStartOptions;
 
-
 /**
  * Close connection to remote exeutor
  *
@@ -59,9 +59,13 @@ Container.prototype.close = close;
  * @param {function} callback fn(err)
  */
 function setStartOptions(options, callback) {
+  if (this.listeners('start-options-updated').count > 1)
+    return callback(
+      Error('only one listener is supported for start-options-updated event'));
+
   if (!lodash.isEqual(options, this._containerOptions)) {
     this._containerOptions = options;
-    this.emit('start-options-updated', this, callback);
+    return this.emit('start-options-updated', this, callback);
   }
   process.nextTick(callback);
 }
@@ -75,21 +79,30 @@ Container.prototype.setStartOptions = setStartOptions;
  * @param {function} callback fn(err)
  */
 function setEnv(env, callback) {
+  if (this.listeners('env-updated').count > 1)
+    return callback(
+      Error('only one listener is supported for env-updated event'));
+
   env = lodash.mapValues(env, function(x) {
     return String(x);
   });
 
   if (!lodash.isEqual(env, this._env)) {
     this._env = env;
-    this.emit('env-updated', this, callback);
+    return this.emit('env-updated', this, callback);
   }
   process.nextTick(callback);
 }
 Container.prototype.setEnv = setEnv;
 
+function updateContainerMetadata(metadata, callback) {
+  this._server.setInstanceMetadata(this._id, metadata, callback);
+}
+Container.prototype.updateContainerMetadata = updateContainerMetadata;
+
 function _onNotification(msg, callback) {
   debug('Supervisor message: %j', msg);
-  callback({});
+  this._server.onInstanceNotification(this._id, msg, callback);
 }
 Container.prototype._onNotification = _onNotification;
 
